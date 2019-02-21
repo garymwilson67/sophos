16cc430946ba7a99fcacd939260d051257720b27
garymwilson67 <gary.w@quali.com>
2019-02-21 17:44:23 +0000

    Add files via upload

---
+++ CreateTrainingVMs.ps1
@@ -0,0 +1,5 @@
+﻿Write-Output "Creating New VM Switch in Hyper-V";
+New-VMSwitch -name InternalSwitch -SwitchType Internal;
+Write-Output "Creating New Training VMs: TrainingVM1 and TrainingVM2";
+New-VM -Name TrainingVM1 -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath .\VMs\Win10_1.vhdx -Path .\VMData -NewVHDSizeBytes 20GB -Generation 2 -Switch InternalSwitch;
+New-VM -Name TrainingVM2 -MemoryStartupBytes 4GB -BootDevice VHD -NewVHDPath .\VMs\Win10_2.vhdx -Path .\VMData -NewVHDSizeBytes 20GB -Generation 2 -Switch InternalSwitch;
